fac65297c2dbecd1911af57d2e90bd90445d2800
Enderser <enderser4@gmail.com>
2025-03-11 02:58:24 +0700

    Refresh task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,9 +1,8 @@
-from ping3 import ping, verbose_ping
-import socket
 import csv
+import socket
 
-def my_ping(host):
-    return socket.gethostbyname(host), ping(host)
+from ping3 import ping, verbose_ping
+
 
 hosts = ['google.com', 'yandex.ru', 'youtube.com', 'facebook.com', 'wikipedia.org', 
         'amazon.com', 'reddit.com', 'vk.com', 'pinterest.com', 'ebay.com']
@@ -11,7 +10,7 @@ with open('task1.csv', 'w') as table:
     csvwriter = csv.writer(table)
     csvwriter.writerow(['Host', 'IP', 'RTT'])
     for host in hosts:
-        IP, RTT = my_ping(host)
+        IP, RTT = socket.gethostbyname(host), ping(host)
         csvwriter.writerow([host, IP, RTT])
         print(host)
         verbose_ping(host)
